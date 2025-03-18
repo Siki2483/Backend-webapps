e52aef523dbd80cb4c21a2ae1329cb97568609c4
Siki <josip6291@gmail.com>
2025-03-18 16:49:34 +0100

    Dodan login za usera

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ const User = require("../models/User");
 
 const router = express.Router();
 
+// REGISTER
+
 router.post(
   "/register",
   [
@@ -62,5 +64,59 @@ router.post(
     }
   }
 );
+
+// LOGIN
+
+router.post(
+  "/login",
+  [
+    check("email", "Enter a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid email or password" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid email or password" });
+      }
+
+      // Generiranje JWT tokena
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+
 
 module.exports = router;
