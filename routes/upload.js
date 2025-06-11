eae2b255f52149019a70326c15a70be95f4e9f10
Siki <josip6291@gmail.com>
2025-06-11 12:12:37 +0200

    dodavanje cloudinary za slike

--- routes/upload.js
+++ routes/upload.js
@@ -1,30 +1,12 @@
 const express = require("express");
 const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
-
+const { storage } = require("../utils/cloudinary");
 const router = express.Router();
-
-
-const uploadDir = path.join(__dirname, "public/uploads");
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "public/uploads");
-  },
-  filename(req, file, cb) {
-    cb(null, `${Date.now()}${path.extname(file.originalname)}`);
-  },
-});
 
 const upload = multer({ storage });
 
 router.post("/", upload.single("image"), (req, res) => {
-  res.send({ imagePath: `/uploads/${req.file.filename}` });
+  res.send({ imagePath: req.file.path });
 });
 
 module.exports = router;
